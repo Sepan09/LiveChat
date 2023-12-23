4388c2bdd115320ab0cb9ec0b890cb643ab42a34
sepan mohammed <sepanmustafa@gmail.com>
2023-12-22 20:26:39 -0600

    Created helpers.js and created css folder and js folder in public folder.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers')
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
